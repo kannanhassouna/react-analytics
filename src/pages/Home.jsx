3a43b22942a3fe20bd4e7a9fc08d0eee283540bf
kannanhassouna <kannan.m.hassouna@gmail.com>
2024-12-02 02:22:40 +0100

    introduce feature-flags

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,9 @@
-export default function Home(){
-    return <h1>Home Page</h1>
+import { useFeatureFlagEnabled } from "posthog-js/react";
+
+export default function Home() {
+  const flagEnabled = useFeatureFlagEnabled("new-landing-page");
+  if (flagEnabled) {
+    return <h1>New Landing Page</h1>;
+  }
+  return <h1>Home Page</h1>;
 }
